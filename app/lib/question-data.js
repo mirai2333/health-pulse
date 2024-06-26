4fe70fcc9da5cf51c701531851752e84d83a4944
Tornado <973593589@qq.com>
2024-06-26 17:55:30 +0800

    又搞了一波选项，加优化

--- app/lib/question-data.js
+++ app/lib/question-data.js
@@ -8,6 +8,22 @@ const questionData = [
     { id: "7", pid: "1", oid: "", parent: "2", text: "您是已就医还是未就医？" },
     { id: "8", pid: "", oid: "7", parent: "", text: "已就医咨询" },
     { id: "9", pid: "", oid: "7", parent: "", text: "未就医咨询" },
+
+    { id: "10", pid: "7", oid: "", parent: "9", text: "请选择什么问题？" },
+    { id: "11", pid: "", oid: "10", parent: "", text: "挂号问题" },
+    { id: "12", pid: "", oid: "10", parent: "", text: "医保目录问题" },
+    { id: "13", pid: "", oid: "10", parent: "", text: "医保报销问题" },
+
+    { id: "14", pid: "10", oid: "", parent: "11", text: "对挂号有什么疑问？" },
+    { id: "15", pid: "", oid: "14", parent: "", text: "如何挂号" },
+    { id: "16", pid: "", oid: "14", parent: "", text: "如何选择就诊科室" },
+
+    { id: "17", pid: "7", oid: "", parent: "8", text: "您的参保类型是什么？" },
+    { id: "18", pid: "", oid: "17", parent: "", text: "广州医保" },
+    { id: "19", pid: "", oid: "17", parent: "", text: "市直医保" },
+    { id: "20", pid: "", oid: "17", parent: "", text: "省直医保" },
+    { id: "21", pid: "", oid: "17", parent: "", text: "其他类别" },
+    { id: "22", pid: "", oid: "17", parent: "", text: "异地就医" }
 ];
 
 export default questionData;
--- app/ui/questionnaire.js
+++ app/ui/questionnaire.js
@@ -7,15 +7,20 @@ const Questionnaire = ({ questionnaireTree }) => {
     const [currentNode, setCurrentNode] = useState(questionnaireTree);
     const [answers, setAnswers] = useState([]);
     const handleAnswer = (answer) => {
+        console.log("当前点击", answer);
         setAnswers([...answers, answer]);
         if (currentNode.children && currentNode.children.length > 0) {
-            setCurrentNode(currentNode.children.find(item => item.parent === answer)); // 简化逻辑，实际应根据选项决定
+            const tem = currentNode.children.find(item => item.parent === answer);
+            if (tem) setCurrentNode(tem); // 简化逻辑，实际应根据选项决定
+            else alert('问卷结束！');
         } else {
             alert('问卷结束！');
         }
+        console.log("当前节点", currentNode);
     };
 
     const back = () => {
+        if (currentNode.pid === "-1") return;
         const lastNode = findNodeById(questionnaireTree, currentNode.pid);
         setCurrentNode(lastNode);
     }
